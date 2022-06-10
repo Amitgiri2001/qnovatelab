0c1dc29d3c410ba75ef24cd5b572233e1cba4df5
Amit Giri <giriamit987@gmail.com>
2022-06-10 16:55:41 +0530

    first commit

---
+++ Project/full-frontend/src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import './App.css';
+import { Route,Routes } from 'react-router';
+
+import Home from "./Components/Home";
+import About from "./Components/About"
+import Ourprograms from "./Components/Ourprograms";
+import Booksession from "./Components/Bookyourfreesession"
+import Blog from "./Components/Blog";
+import Registration from "./Components/Registration";
+
+function App() {
+  return (
+    <div className="App">
+    <Routes>
+    <Route path="/" element={<Home/>}/>
+    <Route path="/about" element={<About/>}/>
+   <Route path="/ourprograms" element={<Ourprograms/>}/>
+   <Route path="/bookyourfreesession" element={<Booksession/>}/>
+   <Route path="/blog" element={<Blog/>}/>
+   <Route path="/registration" element={<Registration/>}/>
+    </Routes>
+    </div>
+  );
+}
+
+export default App;
